631b9db23fa9b230e9cb14ded3b3c6099092f494
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-09-21 19:08:54 +0530

    Removed light mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:Tasks/Screens/HomeScreen.dart';
+import 'package:Tasks/View/HomeScreen.dart';
 import 'package:Tasks/widgets/themes.dart';
 import 'package:Tasks/services/notification_service.dart';
 import 'package:timezone/data/latest.dart' as tz;
@@ -15,8 +15,8 @@ void main() async {
   runApp(GetMaterialApp(
     home: const HomeScreen(),
     debugShowCheckedModeBanner: false,
-    theme: Themes.light,
+    theme: Themes.dark,
     darkTheme: Themes.dark,
-    themeMode: ThemeMode.system,
+    themeMode: ThemeMode.dark,
   ));
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -6,7 +6,6 @@ import 'package:google_fonts/google_fonts.dart';
 class Themes {
   static final dark = ThemeData.dark().copyWith(
       canvasColor: const Color(0xFF414141),
-      shadowColor: const Color(0xFF414141),
       splashColor: Colors.transparent,
       // ignore: prefer_const_constructors
       checkboxTheme: CheckboxThemeData(
@@ -26,25 +25,69 @@ class Themes {
         headline2: GoogleFonts.notoSans(color: const Color(0xFFEAEAEA)),
         headline3: GoogleFonts.notoSans(color: const Color(0xFFA8A8A8)),
       ));
-
-  static final light = ThemeData.light().copyWith(
-      shadowColor: const Color(0xFF707070),
-      canvasColor: Colors.white,
-      splashColor: Colors.transparent,
-      checkboxTheme: const CheckboxThemeData(
-        side: BorderSide(color: Color(0xFF707070)),
-      ),
-      iconTheme: const IconThemeData(color: Color(0xFF707070)),
-      highlightColor: Colors.transparent,
-      hintColor: const Color(0xFF707070),
-      scaffoldBackgroundColor: const Color(0xFFF0EFEF),
-      appBarTheme:
-          const AppBarTheme(backgroundColor: Color(0xFFF0EFEF), elevation: 0),
-      textTheme: TextTheme(
-        headline1: GoogleFonts.notoSans(
-            fontSize: 30,
-            color: const Color(0xFF707070),
-            fontWeight: FontWeight.bold),
-        headline2: GoogleFonts.notoSans(color: const Color(0xFF707070)),
-      ));
 }
+
+// TextStyles
+
+TextStyle titleStyle = GoogleFonts.notoSans(
+  fontSize: 30,
+  color: const Color(0xFFEAEAEA),
+  fontWeight: FontWeight.bold,
+);
+
+TextStyle optionsTextStyle = GoogleFonts.notoSans(
+  fontSize: 17.0,
+  color: const Color(0xFFEAEAEA),
+);
+
+TextStyle alertTextStyle = GoogleFonts.notoSans(
+  fontSize: 20.0,
+  color: const Color(0xFFEAEAEA),
+);
+
+TextStyle todoTitleStyle(condition) => GoogleFonts.notoSans(
+    color: const Color(0xFFA8A8A8),
+    fontSize: 23.0,
+    decoration: (condition) ? TextDecoration.lineThrough : TextDecoration.none);
+
+TextStyle todoDetailsStyle(condition) => GoogleFonts.notoSans(
+      color: const Color(0xFFA8A8A8),
+      fontSize: 20.0,
+      decoration:
+          (condition) ? TextDecoration.lineThrough : TextDecoration.none,
+    );
+
+TextStyle todoTimeStyle(condition1, condition2) => GoogleFonts.notoSans(
+      color: (condition1) ? const Color(0xFFA8A8A8) : Colors.redAccent,
+      fontSize: 20.0,
+      decoration:
+          (condition2) ? TextDecoration.lineThrough : TextDecoration.none,
+    );
+
+TextStyle buttonTextStyle =
+    const TextStyle(color: Color(0xFFEAEAEA), fontSize: 23.0);
+
+TextStyle menuTextStyle = const TextStyle(fontSize: 22, color: Color(0xFFEAEAEA));
+TextStyle menuTextStyleBlue = const TextStyle(fontSize: 20.0, color: Color(0xFF39A7FD));
+
+TextStyle todoScreenStyle = GoogleFonts.notoSans(
+      color: const Color(0xFFA8A8A8),
+      fontSize: 23.0,
+    );
+
+TextStyle todoScreenDetailsStyle = GoogleFonts.notoSans(
+      color: const Color(0xFFA8A8A8),
+      fontSize: 20.0,
+    );
+//Other styles
+
+Divider dividerStyle = const Divider(
+  color: Color(0xFF707070),
+  thickness: 1.0,
+);
+
+Icon menuIcon = const Icon(
+  Icons.menu,
+  color: Color(0xFFEAEAEA),
+  size: 27.0,
+);
