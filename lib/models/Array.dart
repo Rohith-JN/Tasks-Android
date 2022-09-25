bb6ab93bedc39a8bd3ca77c25c02b846478112de
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-09-25 15:48:46 +0530

    Updated Model

---
+++ lib/models/Array.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names
+import 'package:Tasks/models/Todo.dart';
+
+class Array {
+  String title;
+  int id;
+  List<Todo>? todos;
+  Array({
+    required this.title,
+    required this.id,
+    required this.todos
+  });
+
+  factory Array.fromJson(Map<String, dynamic> json) => Array(
+      id: json['id'],
+      title: json['title'],
+      todos: json['todos']
+      );
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'todos': todos
+      };
+}
