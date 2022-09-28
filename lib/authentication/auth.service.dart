38e41adf13fa3c92405c0fe4be37cc83ffc7ed6e
Rohith JN <78314165+Rohith-JN@users.noreply.github.com>
2022-09-28 10:37:16 +0530

    Added Firebase auth

---
+++ lib/authentication/auth.service.dart
@@ -0,0 +1,99 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tasks/utils/global.dart';
+
+class Authentication {
+  static Future signIn(emailController, passwordController, context) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+    } on FirebaseAuthException catch (e) {
+      final snackBar = SnackBar(
+        backgroundColor: secondaryColor,
+        content: Text(e.message!, style: const TextStyle(color: Colors.white)),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
+  static Future signUp(emailController, passwordController, context) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailController.text, password: passwordController.text);
+    } on FirebaseAuthException catch (e) {
+      final snackBar = SnackBar(
+        backgroundColor: secondaryColor,
+        content: Text(e.message!, style: const TextStyle(color: Colors.white)),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+
+  static Future resetPassword(context, emailController) async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: ((context) =>
+            const Center(child: CircularProgressIndicator())));
+    try {
+      await FirebaseAuth.instance
+          .sendPasswordResetEmail(email: emailController.text);
+      var snackBar = const SnackBar(
+        backgroundColor: secondaryColor,
+        content: Text('Password Reset Email Sent',
+            style: TextStyle(color: Colors.white)),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.of(context).popUntil((route) => route.isFirst);
+    } on FirebaseAuthException catch (e) {
+      final snackBar = SnackBar(
+        backgroundColor: secondaryColor,
+        content: Text(e.message!, style: const TextStyle(color: Colors.white)),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.of(context).pop();
+    }
+  }
+
+  static Future signOut(context) async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: ((context) =>
+            const Center(child: CircularProgressIndicator())));
+    try {
+      await showDialog<String>(
+        barrierDismissible: true,
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          backgroundColor: const Color.fromARGB(255, 37, 37, 37),
+          title: const Text('Sign Out', style: TextStyle(color: Colors.white)),
+          content: const Text('Are you sure you want to sign out?',
+              style: TextStyle(color: Color.fromARGB(255, 187, 187, 187))),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child:
+                  const Text('Cancel', style: TextStyle(color: primaryColor)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, 'Ok');
+                FirebaseAuth.instance.signOut();
+              },
+              child: const Text('OK', style: TextStyle(color: primaryColor)),
+            ),
+          ],
+        ),
+      );
+      Navigator.of(context).popUntil((route) => route.isFirst);
+    } on FirebaseAuthException catch (e) {
+      final snackBar = SnackBar(
+        backgroundColor: secondaryColor,
+        content: Text(e.message!, style: const TextStyle(color: Colors.white)),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.of(context).pop();
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,51 @@
-import 'package:Tasks/View/MainScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:Tasks/widgets/themes.dart';
-import 'package:Tasks/services/notification_service.dart';
+import 'package:tasks/services/Notification.service.dart';
+import 'package:tasks/utils/global.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:tasks/view/LoginScreen.dart';
+import 'package:tasks/view/MainScreen.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
-void main() async {
+Future main() async {
   tz.initializeTimeZones();
   WidgetsFlutterBinding.ensureInitialized();
   NotificationService().initNotification();
   await GetStorage.init();
-  runApp(GetMaterialApp(
-    home: const MainScreen(),
-    debugShowCheckedModeBanner: false,
-    theme: Themes.dark,
-    darkTheme: Themes.dark,
-    themeMode: ThemeMode.dark,
-  ));
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+final navigatorKey = GlobalKey<NavigatorState>();
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      title: 'Tasks',
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.black,
+          appBarTheme: AppBarTheme(color: Colors.black)),
+      home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                  child: Text("Something went wrong!", style: headingWhite));
+            } else if (snapshot.hasData) {
+              return const MainScreen();
+            } else {
+              return const LogIn();
+            }
+          }),
+    );
+  }
 }
